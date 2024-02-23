ac697d0ebe47c78493df9b9866cbe2eacbfb4f01
Tomeu Aloy <r0845813@ucll.be>
2024-02-23 11:48:29 +0100

    Merge branch 'main' of https://github.com/UCLL-PR2/exercises

---
+++ 02-oo/02-properties/03-setters/student.py
@@ -0,0 +1,6 @@
+# Write your code here
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.__hours = hours
+        self.__minutes = minutes
+        self.__seconds = seconds
